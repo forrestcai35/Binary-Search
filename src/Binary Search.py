1f8197c478ef90c686c52a51d93d7884faadb5f3
Forrest Cai <forrestcai35@gmail.com>
2024-11-03 20:51:20 -0500

    Finish algorithm

--- src/Binary Search.py
+++ src/Binary Search.py
@@ -12,12 +12,12 @@ def BS(list, val):
   
   Parameter val: value to look for 
 
-  Returns the index of value 
+  Returns the index of value and None if not found 
   """
   i = 0
   j = len(list) - 1 
 
-  while i < j:
+  while i <= j:
     mid = i + (j-i) // 2
     if list[mid] == val: #The value is in the middle 
       return mid 
@@ -25,11 +25,13 @@ def BS(list, val):
       j = mid - 1
     else:
       i = mid + 1
+
+  return -1 
   
 
 
-list = [1,2,2,2,2,2,2,2,3,4,5,6]
-val = 3 
+list = [1]
+val = 1
 
 print(BS(list,val))
 
